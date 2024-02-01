4eb0ee62c0062533395cc761843af2561cb4b8bd
John <1769064325@qq.com>
2024-02-01 16:45:13 +0800

    清空购物车业务代码开发

--- sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -91,4 +91,17 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
         return list;
     }
 
+    /**
+     * 清空购物车
+     */
+    @Override
+    public void cleanShoppingCart() {
+        //获取当前微信用户的id
+        Long userId = BaseContext.getCurrentId();
+        ShoppingCart shoppingCart = ShoppingCart.builder()
+                                                .userId(userId)
+                                                .build();
+        shoppingCartMapper.deleteByUserId(shoppingCart);
+    }
+
 }
